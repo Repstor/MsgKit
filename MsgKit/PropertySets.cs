7973782d97eaa7fb85f2c47e98a1eab9a90a95c4
Michael Shanks <mjashanks@hotmail.com>
2019-01-28 15:37:49 +0000

    made property set guids public

--- MsgKit/PropertySets.cs
+++ MsgKit/PropertySets.cs
@@ -9,14 +9,14 @@ namespace MsgKit
     /// <summary>
     /// Set of constants - common property set ids - [MS-OXPROPS] 1.3.2
     /// </summary>
-    internal class PropertySets
+    public class PropertySets
     {
-        internal static Guid PS_PUBLIC_STRINGS => new Guid("00020329-0000-0000-C000-000000000046");
-        internal static Guid PS_INTERNET_HEADERS => new Guid("00020386-0000-0000-C000-000000000046");
-        internal static Guid PSETID_Common => new Guid("00062008-0000-0000-C000-000000000046");
-        internal static Guid PS_MAPI => new Guid("00020328-0000-0000-C000-000000000046");
-        internal static Guid PSETID_Task => new Guid("00062003-0000-0000-C000-000000000046");
-        internal static Guid PSETID_Appointment => new  Guid("00062002-0000-0000-C000-000000000046");
-        internal static Guid PSETID_Meeting => new  Guid("6ED8DA90-450B-101B-98DA-00AA003F1305");
+        public static Guid PS_PUBLIC_STRINGS => new Guid("00020329-0000-0000-C000-000000000046");
+        public static Guid PS_INTERNET_HEADERS => new Guid("00020386-0000-0000-C000-000000000046");
+        public static Guid PSETID_Common => new Guid("00062008-0000-0000-C000-000000000046");
+        public static Guid PS_MAPI => new Guid("00020328-0000-0000-C000-000000000046");
+        public static Guid PSETID_Task => new Guid("00062003-0000-0000-C000-000000000046");
+        public static Guid PSETID_Appointment => new  Guid("00062002-0000-0000-C000-000000000046");
+        public static Guid PSETID_Meeting => new  Guid("6ED8DA90-450B-101B-98DA-00AA003F1305");
     }
 }
